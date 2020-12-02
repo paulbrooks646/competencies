5de7a90f2ab9bb57be2b474e181033a910d32ee7
paulbrooks646 <paulbrooks646@gmail.com>
2020-12-02 15:51:47 -0700

    tutorial links

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import Nav from './Components/Nav'
 function App() {
   return (
     <div className="App" style={{width: `100vw`, height: `100vh`, display: 'flex'}}>
-      <div style={{width: '15%'}}>
-      <Nav/>
-      </div>
+
       <div style={{width: '100%'}}>
       {Routes}
       </div>
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,12 +1,42 @@
 import React from "react"
 import { connect } from "react-redux"
-import {getUser} from '../redux/userReducer'
+import { getUser } from '../redux/userReducer'
+import { Link } from "react-router-dom"
 
 function Dashboard(props) {
 
     return (
-        <div><h1>Welcome {props.user.user.username}</h1></div>
-    )
+      <div>
+        <div>
+          <h1>Welcome {props.user.user.username}</h1>
+            </div>
+            <div>
+                <div>
+                    <Link to="/Animation">Animation</Link>
+                    <Link to="/Background">Background</Link>
+                    <Link to="/BoxModel">Box Model</Link>
+                </div>
+                <div>
+                    <Link to="/Color">Color</Link>
+                    <Link to="/Flexbox">Flexbox</Link>
+                    <Link to="/FloatDisplay">Float and Display</Link>
+                </div>
+                <div>
+                    <Link to="/Font">Font</Link>
+                    <Link to="/Grid">Grid</Link>
+                    <Link to="/Position">Postion</Link>
+                </div>
+                <div>
+                    <Link to="/Pseudo">Pseudo</Link>
+                    <Link to="/TextEffects">Text Effects</Link>
+                    <Link to="/Transform">Transform</Link>
+                </div>
+                <div>
+                    <Link to="Transition">Transition</Link>
+                </div>
+            </div>
+      </div>
+    );
 }
 
 const mapStateToProps = reduxState => reduxState
