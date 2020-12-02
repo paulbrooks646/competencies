989715aa09c8801e160b940bef157f2249553e07
paulbrooks646 <paulbrooks646@gmail.com>
2020-12-02 16:15:09 -0700

    dashboard basic structure

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,44 +1,69 @@
-import React from "react"
-import { connect } from "react-redux"
-import { getUser } from '../redux/userReducer'
-import { Link } from "react-router-dom"
+import React from "react";
+import { connect } from "react-redux";
+import { getUser } from "../redux/userReducer";
+import { Link } from "react-router-dom";
 
 function Dashboard(props) {
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h1>Welcome {props.user.user.username}</h1>
-            </div>
-            <div>
-                <div>
-                    <Link to="/Animation">Animation</Link>
-                    <Link to="/Background">Background</Link>
-                    <Link to="/BoxModel">Box Model</Link>
-                </div>
-                <div>
-                    <Link to="/Color">Color</Link>
-                    <Link to="/Flexbox">Flexbox</Link>
-                    <Link to="/FloatDisplay">Float and Display</Link>
-                </div>
-                <div>
-                    <Link to="/Font">Font</Link>
-                    <Link to="/Grid">Grid</Link>
-                    <Link to="/Position">Postion</Link>
-                </div>
-                <div>
-                    <Link to="/Pseudo">Pseudo</Link>
-                    <Link to="/TextEffects">Text Effects</Link>
-                    <Link to="/Transform">Transform</Link>
-                </div>
-                <div>
-                    <Link to="Transition">Transition</Link>
-                </div>
-            </div>
+        <h1>Welcome {props.user.user.username}</h1>
       </div>
-    );
+      <div className="dashboard-links-main">
+        <div className="dashboard-links-row">
+          <Link to="/Animation">
+            <button className="dashboard-link">Animation</button>
+          </Link>
+          <Link to="/Background">
+            <button className="dashboard-link">Background</button>
+          </Link>
+          <Link to="/BoxModel">
+            <button className="dashboard-link">Box Model</button>
+          </Link>
+        </div>
+        <div className="dashboard-links-row">
+          <Link to="/Color">
+            <button className="dashboard-link">Color</button>
+          </Link>
+          <Link to="/Flexbox">
+            <button className="dashboard-link">Flexbox</button>
+          </Link>
+          <Link to="/FloatDisplay">
+            <button className="dashboard-link">Float/Display</button>
+          </Link>
+        </div>
+        <div className="dashboard-links-row">
+          <Link to="/Font">
+            <button className="dashboard-link">Font</button>
+          </Link>
+          <Link to="/Grid">
+            <button className="dashboard-link">Grid</button>
+          </Link>
+          <Link to="/Position">
+            <button className="dashboard-link">Position</button>
+          </Link>
+        </div>
+        <div className="dashboard-links-row">
+          <Link to="/Pseudo">
+            <button className="dashboard-link">Pseudo</button>
+          </Link>
+          <Link to="/TextEffects">
+            <button className="dashboard-link">Text Effects</button>
+          </Link>
+          <Link to="/Transform">
+            <button className="dashboard-link">Transform</button>
+          </Link>
+        </div>
+        <div>
+          <Link to="Transition">
+            <button className="dashboard-link">Transition</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-const mapStateToProps = reduxState => reduxState
+const mapStateToProps = (reduxState) => reduxState;
 
-export default connect(mapStateToProps, {getUser})(Dashboard)
+export default connect(mapStateToProps, { getUser })(Dashboard);
