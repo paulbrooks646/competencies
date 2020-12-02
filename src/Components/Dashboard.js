295f224146a31a7bf840bf6206ef6386e4938bb6
paulbrooks646 <paulbrooks646@gmail.com>
2020-12-02 15:32:39 -0700

    connected dashboard to redux

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,14 @@
 import React from "react"
+import { connect } from "react-redux"
+import {getUser} from '../redux/userReducer'
 
-export default function Dashboard() {
+function Dashboard(props) {
 
     return (
         <div><h1>Dashboard</h1></div>
     )
 }
+
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps, {getUser})(Dashboard)
