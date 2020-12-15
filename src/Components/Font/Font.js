4b6ba7605a5b7d9fb3b8ddb46b7e89bea9b816f1
paulbrooks646 <paulbrooks646@gmail.com>
2020-12-15 12:42:17 -0700

    font finish

--- src/Components/Font/Font.js
+++ src/Components/Font/Font.js
@@ -61,7 +61,9 @@ export default function Font() {
       <div className="font-practice">
         <div className="font-properties">
           <div className="font-input-div">
-            <label className="font-label">Color:</label>
+            <label className="font-label" onClick={() => toggleColorCard()}>
+              Color:
+            </label>
             <input
               className="font-input"
               placeholder="Enter color here"
@@ -69,7 +71,9 @@ export default function Font() {
             />
           </div>
           <div className="font-input-div">
-            <label className="font-label">Font Style:</label>
+            <label className="font-label" onClick={() => toggleStyleCard()}>
+              Font Style:
+            </label>
             <select
               className="font-select"
               onChange={(e) => setStyle(e.target.value)}
@@ -80,7 +84,9 @@ export default function Font() {
             </select>
           </div>
           <div className="font-input-div">
-            <label className="font-label">Font Variant:</label>
+            <label className="font-label" onClick={() => toggleVariantCard()}>
+              Font Variant:
+            </label>
             <select
               className="font-select"
               onChange={(e) => setVariant(e.target.value)}
@@ -90,16 +96,20 @@ export default function Font() {
             </select>
           </div>
           <div className="font-input-div">
-            <label className="font-label">Font Size:</label>
+            <label className="font-label" onClick={() => toggleSizeCard()}>
+              Font Size:
+            </label>
             <input
               className="font-input"
               onChange={(e) => setSize(e.target.value)}
-              placeholder="Enter # in pixels here"
+              placeholder="Enter # in pixels"
             />
           </div>
 
           <div className="font-input-div">
-            <label className="font-label">Font Weight:</label>
+            <label className="font-label" onClick={() => toggleWeightCard()}>
+              Font Weight:
+            </label>
             <select
               className="font-select"
               onChange={(e) => setWeight(e.target.value)}
@@ -120,15 +130,19 @@ export default function Font() {
             </select>
           </div>
           <div className="font-input-div">
-            <label className="font-label">Line Height:</label>
+            <label className="font-label" onClick={() => toggleHeightCard()}>
+              Line Height:
+            </label>
             <input
               className="font-input"
               onChange={(e) => setHeight(e.target.value)}
-              placeholder="Enter # relative to font-size here"
+              placeholder="Enter # here"
             />
           </div>
           <div className="font-input-div">
-            <label className="font-label">Font Family:</label>
+            <label className="font-label" onClick={() => toggleFamilyCard()}>
+              Font Family:
+            </label>
             <select
               className="font-select"
               onChange={(e) => setFamily(e.target.value)}
@@ -158,22 +172,105 @@ export default function Font() {
           </h1>
         </div>
       </div>
+      <div className={`${colorCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Color</h2>
+        <p className="font-card-description">
+          The color property is used to specify an element's text's color. You
+          can use predefined color names, hex codes, rgb, rgba, hsl, and hsla
+          codes to define this properties value.
+        </p>
+        <button className="font-card-button" onClick={() => toggleColorCard()}>
+          CLOSE
+        </button>
+      </div>
+      <div className={`${styleCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Font Style</h2>
+        <p className="font-card-description">
+          The font-style property specifies the style of a text. The possible
+          values are normal, italic or oblique.
+        </p>
+        <button className="font-card-button" onClick={() => toggleStyleCard()}>
+          CLOSE
+        </button>
+      </div>
+      <div className={`${variantCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Font Variant</h2>
+        <p className="font-card-description">
+          The font-variant property allows you to turn text to small-caps. In
+          small-caps, all lowercase letters are converted to uppercase letters.
+          However, the converted uppercase letters have a smaller font-size than
+          the original uppercase letters.
+        </p>
+        <button
+          className="font-card-button"
+          onClick={() => toggleVariantCard()}
+        >
+          CLOSE
+        </button>
+      </div>
+      <div className={`${weightCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Font Weight</h2>
+        <p className="font-card-description">
+          The font-weight property sets how thick the characters in a text
+          should be displayed. It accepts the values normal, bold, bolder,
+          lighter or a number between 100 and 900.
+        </p>
+        <button className="font-card-button" onClick={() => toggleWeightCard()}>
+          CLOSE
+        </button>
+      </div>
+      <div className={`${sizeCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Font Size</h2>
+        <p className="font-card-description">
+          The font-size property sets the size of the characters of a text.The
+          values accepted for this property include a variety of preset keywords
+          (which you can see in the dropdown below), fixed lengths defined by
+          units of measurement, or a percentage of the parent elements
+          font-size.
+        </p>
+        <button className="font-card-button" onClick={() => toggleSizeCard()}>
+          CLOSE
+        </button>
+      </div>
+      <div className={`${heightCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Line Height</h2>
+        <p className="font-card-description">
+          The line-height property sets the height of the line containing the
+          specified element's text. This is used to set the distance between
+          lines of text. This property accepts numbers to be multiplied by the
+          text's font-size, a length specified by a unit of measurement, or a
+          percentage of the current font-size.
+        </p>
+        <button className="font-card-button" onClick={() => toggleHeightCard()}>
+          CLOSE
+        </button>
+      </div>
+      <div className={`${familyCard ? "font-card" : "font-card-closed"}`}>
+        <h2 className="font-card-title">Font Family</h2>
+        <p className="font-card-description">
+          The font-family specifies the font for the text of an
+          element. This is the name of a font-family or of a
+          generic family. You may assign multiple values to this property. A
+          value will only be applied if any preceding values are not supported
+          by the browser. A popular place to find free fonts is <a href="fonts.google.com">fonts.google.com</a>
+        </p>
+        <button className="font-card-button" onClick={() => toggleFamilyCard()}>
+          CLOSE
+        </button>
+      </div>
 
       {/* <div style={{ borderBottom: "solid" }}>
         <div className="animationDescriptionRow">
           <div className="fifty">
             <h2>Color</h2>
             <p>
-              The color property is used to specify an element's text's color.
-              You can use predefined color names, hex codes, rgb, rgba, hsl, and
-              hsla codes to define this properties value.
+              
             </p>
           </div>
           <div className="fifty">
             <h2>Font Style</h2>
             <p>
-              The font-style property specifies the style of a text. The
-              possible values are normal, italic or oblique.
+              
             </p>
           </div>
         </div>
@@ -181,18 +278,13 @@ export default function Font() {
           <div className="fifty">
             <h2>Font Variant</h2>
             <p>
-              The font-variant property allows you to turn text to small-caps.
-              In small-caps, all lowercase letters are converted to uppercase
-              letters. However, the converted uppercase letters have a smaller
-              font-size than the original uppercase letters.
+              
             </p>
           </div>
           <div className="fifty">
             <h2>Font Weight</h2>
             <p>
-              The font-weight property sets how thick the characters in a text
-              should be displayed. It accepts the values normal, bold, bolder,
-              lighter or a number between 100 and 900.
+              
             </p>
           </div>
         </div>
@@ -200,21 +292,13 @@ export default function Font() {
           <div className="fifty">
             <h2>Font Size</h2>
             <p>
-              The font-size property sets the size of the characters of a
-              text.The values accepted for this property include a variety of
-              preset keywords (which you can see in the dropdown below), fixed
-              lengths defined by units of measurement, or a percentage of the
-              parent elements font-size.
+              
             </p>
           </div>
           <div className="fifty">
             <h2>Line Height</h2>
             <p>
-              The line-height property sets the height of the line containing
-              the specified element's text. This is commonly used to set the
-              distance between lines of text. This property accepts numbers that
-              will be multiplied by the text's font-size, a length specified by
-              a unit of measurement, or a percentage of the current font-size.
+              
             </p>
           </div>
         </div>
@@ -222,12 +306,7 @@ export default function Font() {
           <div className="fifty">
             <h2>Font Family</h2>
             <p>
-              The font-family property specifies the font for the text of an
-              element. This can be the name of a font-family or the name of a
-              generic family. You may assign multiple values to this property. A
-              value will only be applied if any preceding values are not
-              supported by the browser. A popular place to find free fonts is{" "}
-              <a href="fonts.google.com">fonts.google.com</a>
+              
             </p>
           </div>
         </div>
